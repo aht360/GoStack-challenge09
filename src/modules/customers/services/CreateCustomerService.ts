fc4e01da792b9b31ed8ae8ed390eae20e1182e22
Augusto Terra <ahterra@MacBook-Air-de-Augusto.local>
2021-02-25 15:52:42 -0300

    initial commit

---
+++ src/modules/customers/services/CreateCustomerService.ts
@@ -0,0 +1,33 @@
+import { inject, injectable } from 'tsyringe';
+
+import AppError from '@shared/errors/AppError';
+
+import Customer from '../infra/typeorm/entities/Customer';
+import ICustomersRepository from '../repositories/ICustomersRepository';
+
+interface IRequest {
+  name: string;
+  email: string;
+}
+
+@injectable()
+class CreateCustomerService {
+  constructor(
+    @inject('CustomersRepository')
+    private customersRepository: ICustomersRepository,
+  ) {}
+
+  public async execute({ name, email }: IRequest): Promise<Customer> {
+    const findEmail = await this.customersRepository.findByEmail(email);
+
+    if (findEmail) {
+      throw new AppError('Email in use');
+    }
+
+    const customer = await this.customersRepository.create({ name, email });
+
+    return customer;
+  }
+}
+
+export default CreateCustomerService;
---
+++ src/modules/orders/services/FindOrderService.ts
@@ -0,0 +1,36 @@
+import { inject, injectable } from 'tsyringe';
+
+import AppError from '@shared/errors/AppError';
+
+import IProductsRepository from '@modules/products/repositories/IProductsRepository';
+import ICustomersRepository from '@modules/customers/repositories/ICustomersRepository';
+import Order from '../infra/typeorm/entities/Order';
+import IOrdersRepository from '../repositories/IOrdersRepository';
+
+interface IRequest {
+  id: string;
+}
+
+@injectable()
+class FindOrderService {
+  constructor(
+    @inject('OrdersRepository')
+    private ordersRepository: IOrdersRepository,
+
+    @inject('ProductsRepository')
+    private productsRepository: IProductsRepository,
+
+    @inject('CustomersRepository')
+    private customersRepository: ICustomersRepository,
+  ) {}
+
+  public async execute({ id }: IRequest): Promise<Order | undefined> {
+    const order = await this.ordersRepository.findById(id);
+    if (!order) {
+      throw new AppError('Order not found', 404);
+    }
+    return order;
+  }
+}
+
+export default FindOrderService;
